4ef8c125cb8e58ed7b75cb6ff979a82c4c9d9b20
leaane <leaneche2004@gmail.com>
2023-12-16 14:49:27 -0800

    edit tester

--- src/GameTester.java
+++ src/GameTester.java
@@ -1,6 +1,5 @@
 //File Name: GameTester.java
 //Authors: Leane Che
-//Email Address: lche2@go.pasadena.edu
 //Assignment: Memory Card Matching Game
 //Description: Tests memory card matching game
 //Last date changed: December 10, 2023
